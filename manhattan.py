b2ef14c6f8f7a0d85cbcbeb2bd1d2969e30b39b8
chvjak <chvjak@gmail.com>
2019-05-02 22:12:37 +0200

    RE

--- manhattan.py
+++ manhattan.py
@@ -14,29 +14,75 @@ def is_intersecting(rect1, rect2):
 def get_intersection(rect1, rect2):
     return ((max(rect1[0][0], rect2[0][0]), max(rect1[0][1], rect2[0][1])), (min(rect1[1][0], rect2[1][0]), min(rect1[1][1], rect2[1][1])))
 
+from functools import reduce
+def get_intersections_rect(rectangles_by_lx, rectangles_by_rx, global_max_intersections_key_pair):
+    active_rectangles = []
+    for i in range(global_max_intersections_key_pair[0] + 1):
+        if len(rectangles_by_lx[i]) > 0:
+            active_rectangles += rectangles_by_lx[i]
+
+        if i == global_max_intersections_key_pair[0]:
+            keys = set()
+            segments_l = defaultdict(list)
+            segments_r = defaultdict(list)
+
+            for rect in active_rectangles:
+                l_key, r_key = rect[0][1], rect[1][1]
+                segments_l[l_key].append(rect)
+                segments_r[r_key].append(rect)
+                keys |= set([l_key, r_key])
+
+            keys_list = list(keys)
+            keys_list.sort()
+
+            active_segments = []
+            for key in keys_list:
+                if len(segments_l[key]):
+                    active_segments += segments_l[key]
+
+                if key == global_max_intersections_key_pair[1]:
+                    res = reduce(get_intersection, active_segments)
+                    return res
+
+                for segment in segments_r[i]:
+                    active_segments.remove(segment)
+
+        for rect in rectangles_by_rx[i]:
+            active_rectangles.remove(rect)
+
+
+# 2D scanline!
+from collections import defaultdict
 def count_intersections(active_rectangles):
-    bit_len = len(active_rectangles)
-    max_rect_count = 0
+    segments_l = defaultdict(list)
+    segments_r = defaultdict(list)
 
-    # 2^N :(
-    for i in range(2 ** bit_len):       
-        intersection = None
-        rect_count = 1
-        for j in range(0, bit_len):
-            if 2 ** j & i == 2 ** j:
-                if intersection is not None and is_intersecting(intersection ,  active_rectangles[j]):
-                    intersection = get_intersection(intersection, active_rectangles[j])
-                    rect_count += 1
-                elif 2 ** j & i == 2 ** j and intersection is None:
-                    intersection = active_rectangles[j]
-                else:
-                    break
+    keys = set()
+    for rect in active_rectangles:
+       l_key, r_key = rect[0][1], rect[1][1]
+       segments_l[l_key].append(rect)
+       segments_r[r_key].append(rect)
+       keys |= set([l_key, r_key])
 
-        if rect_count > max_rect_count :
-            max_rect_count = rect_count 
-            max_intersection = intersection if intersection is not None else active_rectangles[0]
+    keys_list = list(keys)
+    keys_list.sort()
 
-    return  max_rect_count, max_intersection
+    active_segments = []
+    max_intersections = 0
+    max_intersection_key = -1
+    for key in keys_list:
+        if len(segments_l[key]):
+            active_segments += segments_l[key]
+
+            if max_intersections < len(active_segments):
+                max_intersections = len(active_segments)
+                max_intersection_key = key
+
+        for segment in segments_r[key]:
+            active_segments.remove(segment)
+
+    return  max_intersections, max_intersection_key 
+
 
 def maximum_overlap_rectangle(rectangles_by_lx, rectangles_by_rx):
     MAX_XY = len(rectangles_by_lx)
@@ -47,17 +93,16 @@ def maximum_overlap_rectangle(rectangles_by_lx, rectangles_by_rx):
     for i in range(MAX_XY):
         if len(rectangles_by_lx[i]) > 0:
             active_rectangles += rectangles_by_lx[i]
-            max_intersections, max_intersections_rect = count_intersections(active_rectangles)
+            max_intersections, max_intersections_key = count_intersections(active_rectangles)
 
             if max_intersections > global_max_intersections:
                 global_max_intersections = max_intersections 
-                global_max_intersections_rect = max_intersections_rect
+                global_max_intersections_key_pair = (i, max_intersections_key)
 
         for rect in rectangles_by_rx[i]:
             active_rectangles.remove(rect)
 
-
-    return global_max_intersections_rect 
+    return get_intersections_rect(rectangles_by_lx, rectangles_by_rx, global_max_intersections_key_pair) 
 
 
 def create_rect(x, y, heading):
@@ -98,7 +143,6 @@ for i in range(T):
         rectangles_by_lx[rectangle[0][0]].append(rectangle)
         rectangles_by_rx[rectangle[1][0]].append(rectangle)
 
-    res_rect = maximum_overlap_rectangle(rectangles_by_lx, rectangles_by_rx)
-          
+    res_rect = maximum_overlap_rectangle(rectangles_by_lx, rectangles_by_rx)         
 
     print("Case #{0}: {1} {2}".format(i + 1, res_rect[0][0], res_rect[0][1]))
