a39f8fca352e88fcb86ce128553819b68a83bd41
chvjak <chvjak@gmail.com>
2019-05-03 22:31:20 +0200

    OK, TLE

--- swordfight.py
+++ swordfight.py
@@ -4,12 +4,36 @@ def input():
     return f.readline()
 # LOCAL
 #------------------------------------------------------------------------------------------------
+import heapq
+class Item:
+    def __init__(self, value):
+        self.value = value
+        self.deleted = False
 
-def is_fair_fight(CHARLES_SWORDS, DALILA_SWORDS, MAX_SKILL_DIFF):
-    return  abs(max(CHARLES_SWORDS) - max(DALILA_SWORDS)) <= MAX_SKILL_DIFF
+    def __lt__(self, value):
+        return True
 
+    def __repr__(self):
+        return "({0}, {1})".format(self.value, self.deleted)
+
+class MaxPQ:
+    def __init__(self):
+        self.data = []
+
+    def push(self, value):
+        item = Item(value)
+        heapq.heappush(self.data, (-value, item))
+
+        return item 
+
+    def max(self):
+        while self.data[0][1].deleted:
+            heapq.heappop(self.data)
+
+        return -self.data[0][0]
 
 import sys
+
 T = int(input())
 for i in range(T):
     SWORD_COUNT, MAX_SKILL_DIFF =  [int(x) for x in input().strip().split(" ")]
@@ -18,8 +42,30 @@ for i in range(T):
 
     result = 0
 
-    for j in range(SWORD_COUNT):
-        for k in range(j + 1, SWORD_COUNT + 1):
-                result += 1 if is_fair_fight(CHARLES_SWORDS[j:k], DALILA_SWORDS[j:k], MAX_SKILL_DIFF) else 0
+    for win_len in range(1, SWORD_COUNT + 1):
+        charles_choices = MaxPQ()
+        dalila_choices = MaxPQ()
+        charles_items = []
+        dalila_items = []
+        for pos in range(win_len - 1):
+            ci = charles_choices.push(CHARLES_SWORDS[pos]) 
+            charles_items.append(ci)
+
+            di = dalila_choices.push(DALILA_SWORDS[pos]) 
+            dalila_items.append(di)
+
+        for pos in range(win_len - 1, SWORD_COUNT):
+            ci = charles_choices.push(CHARLES_SWORDS[pos]) 
+            charles_items.append(ci)
+
+            di = dalila_choices.push(DALILA_SWORDS[pos]) 
+            dalila_items.append(di)
+
+            if abs(charles_choices.max() - dalila_choices.max()) <= MAX_SKILL_DIFF:
+                result += 1 
+
+            charles_items[pos - win_len + 1].deleted = True
+            dalila_items[pos - win_len + 1].deleted = True
+
 
     print("Case #{0}: {1} ".format(i + 1, result))
